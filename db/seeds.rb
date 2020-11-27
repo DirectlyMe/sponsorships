fb646dcbc86b9490a995d7c2a2dd1dfca69c30d9
Jack Hansen <hansenja2011@gmail.com>
2020-11-27 12:13:30 -0700

    add all files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# create a test sponsor and sponsee
+sponsor = Sponsor.create first_name: 'Jack', last_name: 'Testa'
+sponsee = Sponsee.create first_name: 'Pete', last_name: 'Fleb'
+# create an association between the sponsor and sponsee
+Sponsorship.create sponsor_id: sponsor.id, sponsee_id: sponsee.id
+
+# give the sponsor a service
+service = Service.create name: 'Free Room'
+# associate the service with the sponsor
+SponsorService.create sponsor_id: sponsor.id, service_id: service.id
